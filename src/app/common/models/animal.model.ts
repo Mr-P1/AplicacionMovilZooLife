03f824871dd193217b8f196c53db435b1fbb6cb2
diormazabal <164242847+diormazaball@users.noreply.github.com>
2024-09-28 13:52:27 -0300

    cambios home

--- src/app/common/models/animal.model.ts
+++ src/app/common/models/animal.model.ts
@@ -1,12 +1,24 @@
 export interface Animal {
     id: string;
-    nombre_comun: string,
-    nombre_cientifico: string,
-    especie: string,
-    estado: string,
-    posicion_mapa: number,
-    descripcion: string,
-    curiosidad: string,
-    precaucion: string,
-    imagen: string,
+    altura: string; 
+    clase: string; 
+    comportamiento: string; 
+    cuidados: string; 
+    dato_curioso: string; 
+    descripcion_1: string; 
+    descripcion_2: string; 
+    descripcion_3: string; 
+    dieta: string; 
+    disponibilidad: string;
+    estado_conservacion: string; 
+    habitat: string; 
+    imagen: string; 
+    nombre_cientifico: string; 
+    nombre_comun: string;
+    peso: string; 
+    posicion_mapa: number;
+    precaucion_1: string; 
+    precaucion_2: string;
+    precaucion_3: string;
+    zona: string; 
     }
