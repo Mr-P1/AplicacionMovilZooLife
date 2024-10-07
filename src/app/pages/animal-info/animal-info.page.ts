662759f0bab7288a3ba1e479be7558db7e9880f0
diormazabal <164242847+diormazaball@users.noreply.github.com>
2024-10-07 18:31:02 -0300

    mejora
    - Perfil
    - Info animal

--- src/app/pages/animal-info/animal-info.page.ts
+++ src/app/pages/animal-info/animal-info.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonLoading, IonRow, IonCol, IonModal, IonButton, IonButtons, IonIcon, IonGrid } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonLoading, IonRow, IonCol, IonPopover, IonButton, IonButtons, IonIcon, IonGrid, IonList, IonLabel, IonItem } from '@ionic/angular/standalone';
 import { FirestoreService } from '../../common/services/firestore.service';
 import { Animal } from '../../common/models/animal.model';
 import { Observable } from 'rxjs';
@@ -13,25 +13,16 @@ import { AuthService } from '../../common/services/auth.service';
   templateUrl: './animal-info.page.html',
   styleUrls: ['./animal-info.page.scss'],
   standalone: true,
-  imports: [IonGrid, IonIcon, IonButtons, IonButton, IonModal, IonCol, IonRow, IonLoading, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonContent]
+  imports: [IonItem, IonLabel, IonList, IonGrid, IonIcon, IonButtons, IonButton, IonPopover, IonCol, IonRow, IonLoading, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonContent]
 })
 export class AnimalInfoPage implements OnInit {
-  isCuriosidadModalOpen = false;
-  isPrecaucionModalOpen = false;
   animal$: Observable<Animal | null> | undefined;
+
   constructor(
     private animalService: FirestoreService,
     private route: ActivatedRoute,
     private authService: AuthService,
   ) { }
-
-  setCuriosidadOpen(isOpen: boolean) {
-    this.isCuriosidadModalOpen = isOpen;
-  }
-
-  setPrecaucionOpen(isOpen: boolean) {
-    this.isPrecaucionModalOpen = isOpen;
-  }
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
@@ -41,23 +32,17 @@ export class AnimalInfoPage implements OnInit {
       this.animal$ = this.animalService.getAnimal(id);
 
       if (userId) {
-        // Verificar si el usuario ya vio el animal antes de guardar
         this.animalService.usuarioHaVistoAnimal(userId, id).subscribe(haVisto => {
           if (!haVisto) {
-            // Si no lo ha visto, iniciar un temporizador de 5 segundos
             setTimeout(() => {
               this.animalService.guardarAnimalVisto(userId, id).subscribe({
                 next: () => console.log('Animal visto guardado exitosamente'),
                 error: (error) => console.error('Error al guardar el animal visto', error)
               });
-            }, 5000); // Esperar 5 segundos
+            }, 5000); 
           }
         });
       }
     }
   }
-
-
-
-
 }
